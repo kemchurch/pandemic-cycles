ba84e867d7cce968763dde1dffab7145e82277ec
kemchurch <71055130+kemchurch@users.noreply.github.com>
2022-03-14 17:58:59 -0400

    Update proof_cycle.m

--- SIR/proof_cycle.m
+++ SIR/proof_cycle.m
@@ -1,6 +1,9 @@
 % This script proves Theorem 12. It will request some inputs
 % related to parallel processing. You must have INTLAB installed or the
-% script will fail.
+% script will fail. Note that this does NOT require MATLAB's parallel
+% processing toolbox, and in fact, INTLAB is not compatible with
+% it anway. You will need to manually run multiple MATLAB instances. 
+% Doing it this way is thread safe.
 clear all
 load('candidate_cheb_N16.mat')
 parallel(2) = input('How many threads (n) do you want to run? For a balanced session, choose n that divides 10000. Enter a positive integer. n = ');
